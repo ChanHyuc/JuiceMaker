1ea43cced5835ca2461bd9293bcfa6913e719c83
ChanHyuc <whk1140@gmail.com>
2024-07-31 23:17:11 +0900

    Feat: FruitStore, JuiceMaker 타입 정의

---
+++ JuiceMaker/FruitStore.swift
@@ -0,0 +1,11 @@
+class FruitStore {
+    private var strawberry = 10
+    private var banana = 10
+    private var pineapple = 10
+    private var kiwi = 10
+    private var mango = 10
+    
+    private func useFruit() {
+        
+    }
+}
--- JuiceMaker/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker.swift
@@ -1,1 +1,12 @@
-import Foundation
+enum Juice {
+    case strawberry
+    case banana
+    case kiwi
+    case pineapple
+    case strawberryBanana
+    case mangoKiwi
+}
+
+struct JuiceMaker {
+    private let fruitStore = FruitStore()
+}
