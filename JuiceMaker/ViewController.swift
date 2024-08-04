e3a00a1d3953128a2b5fcc24a81e9bfe6b3b2e81
ChanHyuc <whk1140@gmail.com>
2024-08-04 20:55:52 +0900

    Feat: titleLabel 추가 및 과일 emoji 위치 titleLabel 아래로 변경

--- JuiceMaker/ViewController.swift
+++ JuiceMaker/ViewController.swift
@@ -2,33 +2,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let titleLabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .systemGray5
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.text = "맛있는 쥬스를 만들어 드려요!"
+        return label
+    }()
+    
     private let strawberryEmoji = {
         let label = UILabel()
         label.text = "🍓"
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
     private let bananaEmoji = {
         let label = UILabel()
         label.text = "🍌"
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
     private let kiwiEmoji = {
         let label = UILabel()
         label.text = "🥝"
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
     private let pineappleEmoji = {
         let label = UILabel()
         label.text = "🍍"
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
     private let mangoEmoji = {
         let label = UILabel()
         label.text = "🥭"
+        label.font = UIFont.systemFont(ofSize: 50)
         return label
     }()
     
@@ -36,6 +51,7 @@ class ViewController: UIViewController {
         let stackView = UIStackView(arrangedSubviews: [strawberryEmoji, bananaEmoji, kiwiEmoji, pineappleEmoji, mangoEmoji])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
+        stackView.spacing = 100
         return stackView
     }()
     
@@ -46,11 +62,15 @@ class ViewController: UIViewController {
     }
     
     private func configureUI() {
+        view.addSubview(titleLabel)
         view.addSubview(emojiStackView)
         
         NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor),
+            titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1),
+            titleLabel.heightAnchor.constraint(equalToConstant: 80),
+            emojiStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
             emojiStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            emojiStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 
